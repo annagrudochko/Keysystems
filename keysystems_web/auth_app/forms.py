8125f9dd525975f72900b9a190de61cf06bac913
dgush <dgushch@gmail.com>
2024-07-10 18:55:54 +0400

    back auth pochti

--- keysystems_web/auth_app/forms.py
+++ keysystems_web/auth_app/forms.py
@@ -8,13 +8,24 @@ class AuthBaseForm(forms.Form):
     inn = forms.IntegerField()
 
 
+# проверка пароля
+class PasswordForm(forms.Form):
+    password = forms.CharField()
+
+
 # форма регистрации
 class RegistrationForm(forms.Form):
-    inn = forms.IntegerField(min_value=1000000000, max_value=999999999999)
+    # inn = forms.IntegerField(min_value=1000000000, max_value=999999999999)
+    inn = forms.IntegerField()
     email = forms.CharField()
     fio = forms.CharField()
     tel = forms.CharField()
-    reg_progr = forms.CharField()
+    reg_progr = forms.CharField(empty_value='1')
+
+'''
+2024-07-10 15:49:41 <QueryDict: {'csrfmiddlewaretoken': ['Lgu0Ze4EmLNwFrqrRa94TE3bTKkBKLdP0fDaibD09LEChRAzhtXbLIkdTytlubx4'], 
+'inn': ['5555'], 'reg_progr': [''], 'email': ['ggggg@fffff'], 'fio': ['fggg'], 'tel': ['55555']}
+'''
 
     # class Meta:
     #     model = UserKS
