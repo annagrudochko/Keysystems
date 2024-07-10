8d5e3e2c67b179caa61fd578304fe1aca7798c25
dgush <dgushch@gmail.com>
2024-07-10 10:25:13 +0400

    back auth proc 2

--- keysystems_web/auth_app/views.py
+++ keysystems_web/auth_app/views.py
@@ -1,38 +1,71 @@
 from django.shortcuts import render, redirect
 from django.http.request import HttpRequest
 from django.contrib.auth import login, logout
-
+from django.urls import reverse
 
 from .forms import AuthBaseForm
+from .models import UserKS
 from base_utils import log_error
-from enums import RequestMethod
+from enums import RequestMethod, UserRole
 
 
-# распределяет пользователей
-# def login_view(request: HttpRequest):
-#     if request.user.is_authenticated:
-#         return redirect('index_4_1')
+# Определяет начальную страницу пользователя
+def start_page_redirect(request: HttpRequest):
+    if request.user.is_authenticated and request.user.role == UserRole.CLIENT:
+        return redirect('index_4_1')
+
+    elif request.user.is_authenticated and request.user.role == UserRole.STAFF:
+        return redirect('index_4_1')
+
+    else:
+        return redirect('index_2')
 
 
 # выход
-def start_page_redirect(request):
-    if request.user.is_authenticated:
-        return redirect('login')
-    else:
-        logout(request)
-        return redirect('login')
+def logout_view(request):
+    logout(request)
+    return redirect('index_2')
 
 
-# первая клиентская страница
+# первая клиентская страница. Просит инн
 def index_2(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect('')
+
     log_error(request.method, wt=False)
     input_error = 0
     if request.method == RequestMethod.POST:
-        form = AuthBaseForm(request.POST)
-        if form.is_valid():
-            return redirect('index_2_1')
-        else:
-            input_error = 1
+        return redirect('index_2_1')
+        # log_error(request.POST, wt=False)
+        # form = AuthBaseForm(request.POST)
+        # if form.is_valid():
+        #     user_inn = form.cleaned_data["INN"]
+        #     users_inn = UserKS.objects.filter(inn=user_inn).all()
+        #     log_error(f'{users_inn}', wt=False)
+        #     log_error(f'len(users_inn): {len(users_inn)}', wt=False)
+        #     if len(users_inn) == 0:
+        #         return redirect('index_3_1')
+        #
+        #     elif len(users_inn) > 1:
+        #         return redirect(reverse('index_2_1') + f'?inn={users_inn}')
+        #
+        #     else:
+        #         return redirect('index_2_2')
+        # else:
+        #     input_error = 1
 
     context = {'input_error': input_error}
     return render(request, 'index_2.html', context)
+
+
+def index_2_1(request: HttpRequest):
+    # if request.method == RequestMethod.POST:
+    #     form = AuthUserForm(request.POST)
+    #     if form.is_valid():
+    #         redirect('index_2_1')
+    if request.method == RequestMethod.GET:
+        inn = request.GET['inn']
+        log_error(inn, wt=False)
+
+    context = {'INN': inn}
+    return render(request, 'index_2_1.html', context)
--- keysystems_web/keysystems_web/urls.py
+++ keysystems_web/keysystems_web/urls.py
@@ -1,33 +1,17 @@
-"""
-URL configuration for keysystems_web project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import: from my_app import views
-    2. Add a URL to urlpatterns: path('', views.home, name='home')
-Class-based views
-    1. Add an import: from other_app.views import Home
-    2. Add a URL to urlpatterns: path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
-from auth_app import views as main
+from auth_app import views as reg
 from client_app import views as client
 
 urlpatterns = [
-    path('', main.index_2, name='index_2'),
-    path('index_2', main.index_2, name='index_2'),
-    path('index_2_1', client.index_2_1, name='index_2_1'),
+    path('', reg.index_2, name='index_2'),
+    path('index_2', reg.index_2, name='index_2'),
+    path('index_2_1', reg.index_2_1, name='index_2_1'),
     path('index_2_2', client.index_2_2, name='index_2_2'),
     path('index_3_1', client.index_3_1, name='index_3_1'),
     path('index_3_2', client.index_3_2, name='index_3_2'),
-    path('index_4_1', client.index_3_2, name='index_4_1'),
-    path('index_4_2', client.index_3_2, name='index_4_2'),
+    path('index_4_1', client.index_4_1, name='index_4_1'),
+    path('index_4_2', client.index_4_2, name='index_4_2'),
     path('admin/', admin.site.urls),
 ]
